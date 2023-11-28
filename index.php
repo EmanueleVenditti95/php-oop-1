0636e6923466c3207296e18ec6dfd95d84552aef
EmanueleVenditti95 <vendittiema@gmail.com>
2023-11-28 15:31:04 +0100

    Add - construct function

--- index.php
+++ index.php
@@ -3,5 +3,12 @@ class Production {
     public $title;
     public $language;
     public $rating;
+
+    function __construct($_title, $_language, $_rating)
+    {
+        $this->title = $_title;
+        $this->language = $_language;
+        $this->rating = $_rating;
+    }
 };
 
